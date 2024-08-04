0a28485f665cd7cc37781ebd4e780bc8694359bd
jijunwei <56964585@qq.com>
2024-08-04 14:55:06 +0800

    添加力扣题解

--- src/leetcode/simple/Solution_572.java
+++ src/leetcode/simple/Solution_572.java
@@ -106,3 +106,82 @@ class Solution {
     }
 }
 */
+
+/*
+class Solution {
+    // 时间复杂度 O(n)
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        int hs = getHeight(subRoot);
+        return dfs(root, subRoot, hs).getValue();
+    }
+
+    // 代码逻辑同 104. 二叉树的最大深度
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftH = getHeight(root.left);
+        int rightH = getHeight(root.right);
+        return Math.max(leftH, rightH) + 1;
+    }
+
+    // 100. 相同的树
+    private boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null) {
+            return p == q; // 必须都是 null
+        }
+        return p.val == q.val &&
+               isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
+    }
+
+    // 返回 node 的高度，以及是否找到了 subRoot
+    private Pair<Integer, Boolean> dfs(TreeNode node, TreeNode subRoot, int hs) {
+        if (node == null) {
+            return new Pair<>(0, false);
+        }
+        Pair<Integer, Boolean> left = dfs(node.left, subRoot, hs);
+        Pair<Integer, Boolean> right = dfs(node.right, subRoot, hs);
+        if (left.getValue() || right.getValue()) {
+            return new Pair<>(0, true);
+        }
+        int nodeH = Math.max(left.getKey(), right.getKey()) + 1;
+        return new Pair<>(nodeH, nodeH == hs && isSameTree(node, subRoot));
+    }
+}
+*/
+
+/*
+class Solution {
+
+    // 时间复杂度 O(n)
+    private int subRootDepth = -1;
+    private boolean rootContainsSubRoot = false;
+
+    // 判断 a b 两棵树是否相等
+    private boolean equals(TreeNode a, TreeNode b) {
+        if(a == null && b == null) return true;
+        if(a == null || b == null) return false;
+        return a.val == b.val && equals(a.left, b.left) && equals(a.right, b.right);
+    }
+
+    // 计算树的深度
+    private int getDepth(TreeNode node, TreeNode subRoot) {
+        if(rootContainsSubRoot || node == null) return 0;
+        int depth = Math.max(getDepth(node.left, subRoot), getDepth(node.right, subRoot)) + 1;
+
+        // subRootDepth 的初值为 -1，depth >= 0
+        // 当 depth == subRootDepth 时，说明如果 subRoot 的深度已经计算得出
+        // 再看 node 和 subRoot 两棵树是否相等
+        rootContainsSubRoot |= depth == subRootDepth && equals(node, subRoot);
+
+        return depth;
+    }
+
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        subRootDepth = getDepth(subRoot, subRoot);
+        getDepth(root, subRoot);
+        return rootContainsSubRoot;
+    }
+}
+*/
