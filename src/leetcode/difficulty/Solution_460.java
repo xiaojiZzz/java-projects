d8e02bf10128481aa01288f20422fa0fb481606d
jijunwei <56964585@qq.com>
2024-08-06 14:04:35 +0800

    添加力扣题解

--- src/leetcode/difficulty/Solution_460.java
+++ src/leetcode/difficulty/Solution_460.java
@@ -4,12 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeSet;
 
-
 /**
+ * LFU 缓存
  * 请你为 最不经常使用（LFU）缓存算法设计并实现数据结构。
- *
  * 实现 LFUCache 类：
- *
  * LFUCache(int capacity) - 用数据结构的容量 capacity 初始化对象
  * int get(int key) - 如果键 key 存在于缓存中，则获取键的值，否则返回 -1 。
  * void put(int key, int value) - 如果键 key 已存在，则变更其值；如果键不存在，请插入键值对。当缓存达到其容量 capacity 时，
@@ -30,19 +28,24 @@ import java.util.TreeSet;
  * lfu.put(1, 1);   // cache=[1,_], cnt(1)=1
  * lfu.put(2, 2);   // cache=[2,1], cnt(2)=1, cnt(1)=1
  * lfu.get(1);      // 返回 1
- *                  // cache=[1,2], cnt(2)=1, cnt(1)=2
+ * // cache=[1,2], cnt(2)=1, cnt(1)=2
  * lfu.put(3, 3);   // 去除键 2 ，因为 cnt(2)=1 ，使用计数最小
- *                  // cache=[3,1], cnt(3)=1, cnt(1)=2
+ * // cache=[3,1], cnt(3)=1, cnt(1)=2
  * lfu.get(2);      // 返回 -1（未找到）
  * lfu.get(3);      // 返回 3
- *                  // cache=[3,1], cnt(3)=2, cnt(1)=2
+ * // cache=[3,1], cnt(3)=2, cnt(1)=2
  * lfu.put(4, 4);   // 去除键 1 ，1 和 3 的 cnt 相同，但 1 最久未使用
- *                  // cache=[4,3], cnt(4)=1, cnt(3)=2
+ * // cache=[4,3], cnt(4)=1, cnt(3)=2
  * lfu.get(1);      // 返回 -1（未找到）
  * lfu.get(3);      // 返回 3
- *                  // cache=[3,4], cnt(4)=1, cnt(3)=3
+ * // cache=[3,4], cnt(4)=1, cnt(3)=3
  * lfu.get(4);      // 返回 4
- *                  // cache=[3,4], cnt(4)=2, cnt(3)=3
+ * // cache=[3,4], cnt(4)=2, cnt(3)=3
+ * 提示：
+ * 1 <= capacity <= 104
+ * 0 <= key <= 105
+ * 0 <= value <= 109
+ * 最多调用 2 * 105 次 get 和 put 方法
  */
 public class Solution_460 {
     class LFUCache {
@@ -54,14 +57,11 @@ public class Solution_460 {
         public LFUCache(int capacity) {
             this.capacity = capacity;
             this.time = 0;
-            key_table = new HashMap<Integer, Node>();
-            S = new TreeSet<Node>();
+            key_table = new HashMap<>();
+            S = new TreeSet<>();
         }
 
         public int get(int key) {
-            if (capacity == 0) {
-                return -1;
-            }
             // 如果哈希表中没有键 key，返回 -1
             if (!key_table.containsKey(key)) {
                 return -1;
@@ -80,9 +80,6 @@ public class Solution_460 {
         }
 
         public void put(int key, int value) {
-            if (capacity == 0) {
-                return;
-            }
             if (!key_table.containsKey(key)) {
                 // 如果到达缓存容量上限
                 if (key_table.size() == capacity) {
---
+++ src/leetcode/medium/Solution_3129.java
@@ -0,0 +1,119 @@
+package leetcode.medium;
+
+/**
+ * 给你 3 个正整数 zero ，one 和 limit 。
+ * 一个二进制数组 arr 如果满足以下条件，那么我们称它是 稳定的 ：
+ * 0 在 arr 中出现次数 恰好 为 zero 。
+ * 1 在 arr 中出现次数 恰好 为 one 。
+ * arr 中每个长度超过 limit 的子数组都 同时 包含 0 和 1 。
+ * 请你返回 稳定 二进制数组的 总 数目。
+ * 由于答案可能很大，将它对 109 + 7 取余 后返回。
+ * 示例 1：
+ * 输入：zero = 1, one = 1, limit = 2
+ * 输出：2
+ * 解释：
+ * 两个稳定的二进制数组为 [1,0] 和 [0,1] ，两个数组都有一个 0 和一个 1 ，且没有子数组长度大于 2 。
+ * 示例 2：
+ * 输入：zero = 1, one = 2, limit = 1
+ * 输出：1
+ * 解释：
+ * 唯一稳定的二进制数组是 [1,0,1] 。
+ * 二进制数组 [1,1,0] 和 [0,1,1] 都有长度为 2 且元素全都相同的子数组，所以它们不稳定。
+ * 示例 3：
+ * 输入：zero = 3, one = 3, limit = 2
+ * 输出：14
+ * 解释：
+ * 所有稳定的二进制数组包括 [0,0,1,0,1,1] ，[0,0,1,1,0,1] ，[0,1,0,0,1,1] ，[0,1,0,1,0,1] ，[0,1,0,1,1,0] ，[0,1,1,0,0,1] ，
+ * [0,1,1,0,1,0] ，[1,0,0,1,0,1] ，[1,0,0,1,1,0] ，[1,0,1,0,0,1] ，[1,0,1,0,1,0] ，
+ * [1,0,1,1,0,0] ，[1,1,0,0,1,0] 和 [1,1,0,1,0,0] 。
+ * 提示：
+ * 1 <= zero, one, limit <= 200
+ */
+public class Solution_3129 {
+    public int numberOfStableArrays(int zero, int one, int limit) {
+        int mod = 1000000007;
+        // dp[i][j][k] 表示最后一个是 k 的情况下，zero 为 i 个，one 为 j 个 的合法个数
+        int[][][] dp = new int[zero + 1][one + 1][2];
+        for (int i = 1; i <= Math.min(zero, limit); i++) {
+            dp[i][0][0] = 1;
+        }
+        for (int i = 1; i <= Math.min(one, limit); i++) {
+            dp[0][i][1] = 1;
+        }
+        for (int i = 1; i <= zero; i++) {
+            for (int j = 1; j <= one; j++) {
+                // + mod 是因为 dp[i - 1][j][1] + dp[i - 1][j][0] - (i > limit ? dp[i - limit - 1][j][1] : 0) 可能小于 0
+                // 转 long 是因为 dp[i - 1][j][1] + dp[i - 1][j][0] 可能溢出
+                dp[i][j][0] = (int) (((long) dp[i - 1][j][1] + dp[i - 1][j][0] + (i > limit ? mod - dp[i - limit - 1][j][1] : 0)) % mod);
+                dp[i][j][1] = (int) (((long) dp[i][j - 1][0] + dp[i][j - 1][1] + (j > limit ? mod - dp[i][j - limit - 1][0] : 0)) % mod);
+            }
+        }
+        return (dp[zero][one][0] + dp[zero][one][1]) % mod;
+    }
+}
+
+/*
+class Solution {
+    private static final int MOD = 1_000_000_007;
+
+    public int numberOfStableArrays(int zero, int one, int limit) {
+        int[][][] memo = new int[zero + 1][one + 1][2];
+        for (int[][] m : memo) {
+            for (int[] m2 : m) {
+                Arrays.fill(m2, -1); // -1 表示没有计算过
+            }
+        }
+        return (dfs(zero, one, 0, limit, memo) + dfs(zero, one, 1, limit, memo)) % MOD;
+    }
+
+    private int dfs(int i, int j, int k, int limit, int[][][] memo) {
+        if (i == 0) { // 递归边界
+            return k == 1 && j <= limit ? 1 : 0;
+        }
+        if (j == 0) { // 递归边界
+            return k == 0 && i <= limit ? 1 : 0;
+        }
+        if (memo[i][j][k] != -1) { // 之前计算过
+            return memo[i][j][k];
+        }
+        if (k == 0) {
+            // + MOD 保证答案非负
+            memo[i][j][k] = (int) (((long) dfs(i - 1, j, 0, limit, memo) + dfs(i - 1, j, 1, limit, memo) +
+                    (i > limit ? MOD - dfs(i - limit - 1, j, 1, limit, memo) : 0)) % MOD);
+        } else {
+            memo[i][j][k] = (int) (((long) dfs(i, j - 1, 0, limit, memo) + dfs(i, j - 1, 1, limit, memo) +
+                    (j > limit ? MOD - dfs(i, j - limit - 1, 0, limit, memo) : 0)) % MOD);
+        }
+        return memo[i][j][k];
+    }
+}
+*/
+
+/*
+class Solution {
+    static final int MODULO = 1000000007;
+
+    public int numberOfStableArrays(int zero, int one, int limit) {
+        int[][][] dp = new int[zero + 1][one + 1][2];
+        for (int i = Math.min(zero, limit); i >= 0; i--) {
+            dp[i][0][0] = 1;
+        }
+        for (int j = Math.min(one, limit); j >= 0; j--) {
+            dp[0][j][1] = 1;
+        }
+        for (int i = 1; i <= zero; i++) {
+            for (int j = 1; j <= one; j++) {
+                for (int k = 1; k <= limit; k++) {
+                    if (k <= i) {
+                        dp[i][j][0] = (dp[i][j][0] + dp[i - k][j][1]) % MODULO;
+                    }
+                    if (k <= j) {
+                        dp[i][j][1] = (dp[i][j][1] + dp[i][j - k][0]) % MODULO;
+                    }
+                }
+            }
+        }
+        return (dp[zero][one][0] + dp[zero][one][1]) % MODULO;
+    }
+}
+*/
