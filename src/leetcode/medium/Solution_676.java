b2995d33ed423ad13c97a4625c72f27bca622d1a
jijunwei <56964585@qq.com>
2024-08-12 11:46:40 +0800

    每日一题

---
+++ src/leetcode/medium/Solution_676.java
@@ -0,0 +1,165 @@
+package leetcode.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 实现一个魔法字典
+ * 设计一个使用单词列表进行初始化的数据结构，单词列表中的单词 互不相同 。
+ * 如果给出一个单词，请判定能否只将这个单词中一个字母换成另一个字母，使得所形成的新单词存在于你构建的字典中。
+ * 实现 MagicDictionary 类：
+ * MagicDictionary() 初始化对象
+ * void buildDict(String[] dictionary) 使用字符串数组 dictionary 设定该数据结构，dictionary 中的字符串互不相同
+ * bool search(String searchWord) 给定一个字符串 searchWord ，判定能否只将字符串中 一个 字母换成另一个字母，
+ * 使得所形成的新字符串能够与字典中的任一字符串匹配。如果可以，返回 true ；否则，返回 false 。
+ * 示例：
+ * 输入
+ * ["MagicDictionary", "buildDict", "search", "search", "search", "search"]
+ * [[], [["hello", "leetcode"]], ["hello"], ["hhllo"], ["hell"], ["leetcoded"]]
+ * 输出
+ * [null, null, false, true, false, false]
+ * 解释
+ * MagicDictionary magicDictionary = new MagicDictionary();
+ * magicDictionary.buildDict(["hello", "leetcode"]);
+ * magicDictionary.search("hello"); // 返回 False
+ * magicDictionary.search("hhllo"); // 将第二个 'h' 替换为 'e' 可以匹配 "hello" ，所以返回 True
+ * magicDictionary.search("hell"); // 返回 False
+ * magicDictionary.search("leetcoded"); // 返回 False
+ * 提示：
+ * 1 <= dictionary.length <= 100
+ * 1 <= dictionary[i].length <= 100
+ * dictionary[i] 仅由小写英文字母组成
+ * dictionary 中的所有字符串 互不相同
+ * 1 <= searchWord.length <= 100
+ * searchWord 仅由小写英文字母组成
+ * buildDict 仅在 search 之前调用一次
+ * 最多调用 100 次 search
+ */
+public class Solution_676 {
+    class MagicDictionary {
+
+        private Set<String> set;
+
+        public MagicDictionary() {
+            set = new HashSet<>();
+        }
+
+        public void buildDict(String[] dictionary) {
+            for (String s : dictionary) {
+                set.add(s);
+            }
+        }
+
+        public boolean search(String searchWord) {
+            char[] chars = searchWord.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                for (int j = 0; j < 26; j++) {
+                    char c = chars[i];
+                    chars[i] = (char) ('a' + j);
+                    if (chars[i] != c && set.contains(new String(chars))) {
+                        return true;
+                    }
+                    chars[i] = c;
+                }
+            }
+            return false;
+        }
+    }
+}
+
+/*
+class MagicDictionary {
+    private String[] words;
+
+    public MagicDictionary() {
+
+    }
+
+    public void buildDict(String[] dictionary) {
+        words = dictionary;
+    }
+
+    public boolean search(String searchWord) {
+        for (String word : words) {
+            if (word.length() != searchWord.length()) {
+                continue;
+            }
+
+            int diff = 0;
+            for (int i = 0; i < word.length(); ++i) {
+                if (word.charAt(i) != searchWord.charAt(i)) {
+                    ++diff;
+                    if (diff > 1) {
+                        break;
+                    }
+                }
+            }
+            if (diff == 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+*/
+
+/*
+class MagicDictionary {
+    Trie root;
+
+    public MagicDictionary() {
+        root = new Trie();
+    }
+
+    public void buildDict(String[] dictionary) {
+        for (String word : dictionary) {
+            Trie cur = root;
+            for (int i = 0; i < word.length(); ++i) {
+                char ch = word.charAt(i);
+                int idx = ch - 'a';
+                if (cur.child[idx] == null) {
+                    cur.child[idx] = new Trie();
+                }
+                cur = cur.child[idx];
+            }
+            cur.isFinished = true;
+        }
+    }
+
+    public boolean search(String searchWord) {
+        return dfs(searchWord, root, 0, false);
+    }
+
+    private boolean dfs(String searchWord, Trie node, int pos, boolean modified) {
+        if (pos == searchWord.length()) {
+            return modified && node.isFinished;
+        }
+        int idx = searchWord.charAt(pos) - 'a';
+        if (node.child[idx] != null) {
+            if (dfs(searchWord, node.child[idx], pos + 1, modified)) {
+                return true;
+            }
+        }
+        if (!modified) {
+            for (int i = 0; i < 26; ++i) {
+                if (i != idx && node.child[i] != null) {
+                    if (dfs(searchWord, node.child[i], pos + 1, true)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
+
+class Trie {
+    boolean isFinished;
+    Trie[] child;
+
+    public Trie() {
+        isFinished = false;
+        child = new Trie[26];
+    }
+}
+*/
