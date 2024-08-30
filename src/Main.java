10d238288b964edd0bdcd450b0d50586e03c99de
jijunwei <56964585@qq.com>
2024-08-30 09:55:20 +0800

    添加两个模版

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,9 @@
-class Main {
-    public int candy(int[] ratings) {
-        int n = ratings.length;
-        int[] pre = new int[n];
-        int[] post = new int[n];
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                pre[i] = pre[i - 1] + 1;
-            }
-        }
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                post[i] = post[i + 1] + 1;
-            }
-        }
-        int ans = n;
-        for (int i = 0; i < n; i++) {
-            ans += Math.max(pre[i], post[i]);
-        }
-        return ans;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,3 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        int l = 0, r = 1;
-        while (r < n) {
-            if (nums[l] != nums[r]) {
-                nums[l + 1] = nums[r];
-                l++;
-            }
-            r++;
-        }
-        return l + 1;
-    }
+
 }
