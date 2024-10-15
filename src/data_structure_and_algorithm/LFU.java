e0eec0d65fb308dc4416adc3b0aaf9653958aff7
jijunwei <56964585@qq.com>
2024-10-15 11:14:26 +0800

    LFU 淘汰策略

--- src/data_structure_and_algorithm/LFU.java
+++ src/data_structure_and_algorithm/LFU.java
@@ -2,94 +2,104 @@ package data_structure_and_algorithm;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeSet;
 
 // Least Frequently Used（最不经常使用）一种淘汰策略
-public class LFU {
+class LFUCache {
 
-    class Node implements Comparable<Node> {
-        int cnt, time, key, value;
+    class Node {
+        int key, value, freq;
+        Node prev, next;
 
-        Node(int cnt, int time, int key, int value) {
-            this.cnt = cnt;
-            this.time = time;
+        public Node(int key, int value) {
             this.key = key;
             this.value = value;
-        }
-
-        public int compareTo(Node rhs) {
-            return cnt == rhs.cnt ? time - rhs.time : cnt - rhs.cnt;
-        }
-
-        public boolean equals(Object anObject) {
-            if (this == anObject) {
-                return true;
-            }
-            if (anObject instanceof Node) {
-                Node rhs = (Node) anObject;
-                return this.cnt == rhs.cnt && this.time == rhs.time;
-            }
-            return false;
-        }
-
-        // 重写了 equals 需要重写 hashCode
-        public int hashCode() {
-            return cnt * 1000000007 + time;
+            this.freq = 1;
         }
     }
 
-    // 缓存容量，时间戳
-    int capacity, time;
-    Map<Integer, Node> key_table;
-    TreeSet<Node> S;
+    class DoublyLinkedList {
+        Node head, tail;
+        int size;
 
-    public LFU(int capacity) {
+        public DoublyLinkedList() {
+            head = new Node(0, 0);  // Dummy head
+            tail = new Node(0, 0);  // Dummy tail
+            head.next = tail;
+            tail.prev = head;
+            size = 0;
+        }
+
+        public void addLast(Node node) {
+            node.prev = tail.prev;
+            node.next = tail;
+            tail.prev.next = node;
+            tail.prev = node;
+            size++;
+        }
+
+        public void remove(Node node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            size--;
+        }
+
+        public Node removeFirst() {
+            if (size == 0) return null;
+            Node first = head.next;
+            remove(first);
+            return first;
+        }
+    }
+
+    private final int capacity;
+    private int minFreq;
+    private final Map<Integer, Node> keyNodeMap;
+    private final Map<Integer, DoublyLinkedList> freqListMap;
+
+    public LFUCache(int capacity) {
         this.capacity = capacity;
-        this.time = 0;
-        key_table = new HashMap<>();
-        S = new TreeSet<>();
+        this.minFreq = 0;
+        keyNodeMap = new HashMap<>();
+        freqListMap = new HashMap<>();
     }
 
     public int get(int key) {
-        // 如果哈希表中没有键 key，返回 -1
-        if (!key_table.containsKey(key)) {
+        if (!keyNodeMap.containsKey(key)) {
             return -1;
         }
-        // 从哈希表中得到旧的缓存
-        Node cache = key_table.get(key);
-        // 从平衡二叉树中删除旧的缓存
-        S.remove(cache);
-        // 将旧缓存更新
-        cache.cnt += 1;
-        cache.time = ++time;
-        // 将新缓存重新放入哈希表和平衡二叉树中
-        S.add(cache);
-        key_table.put(key, cache);
-        return cache.value;
+        Node node = keyNodeMap.get(key);
+        update(node);
+        return node.value;
     }
 
     public void put(int key, int value) {
-        if (!key_table.containsKey(key)) {
-            // 如果到达缓存容量上限
-            if (key_table.size() == capacity) {
-                // 从哈希表和平衡二叉树中删除最近最少使用的缓存
-                key_table.remove(S.first().key);
-                S.remove(S.first());
-            }
-            // 创建新的缓存
-            Node cache = new Node(1, ++time, key, value);
-            // 将新缓存放入哈希表和平衡二叉树中
-            key_table.put(key, cache);
-            S.add(cache);
+        if (capacity == 0) return;
+
+        if (keyNodeMap.containsKey(key)) {
+            Node node = keyNodeMap.get(key);
+            node.value = value;
+            update(node);
         } else {
-            // 这里和 get() 函数类似
-            Node cache = key_table.get(key);
-            S.remove(cache);
-            cache.cnt += 1;
-            cache.time = ++time;
-            cache.value = value;
-            S.add(cache);
-            key_table.put(key, cache);
+            if (keyNodeMap.size() == capacity) {
+                DoublyLinkedList minList = freqListMap.get(minFreq);
+                Node toRemove = minList.removeFirst();
+                keyNodeMap.remove(toRemove.key);
+            }
+            Node newNode = new Node(key, value);
+            keyNodeMap.put(key, newNode);
+            minFreq = 1;
+            freqListMap.computeIfAbsent(1, k -> new DoublyLinkedList()).addLast(newNode);
         }
+    }
+
+    private void update(Node node) {
+        int freq = node.freq;
+        DoublyLinkedList oldList = freqListMap.get(freq);
+        oldList.remove(node);
+        if (freq == minFreq && oldList.size == 0) {
+            minFreq++;
+        }
+        node.freq++;
+        freqListMap.computeIfAbsent(node.freq, k -> new DoublyLinkedList()).addLast(node);
     }
 }
