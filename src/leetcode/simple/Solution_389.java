9aadac2e5ac0d12124e20fd48fd1bd82a16a67b0
jijunwei <56964585@qq.com>
2024-10-19 23:04:14 +0800

    添加力扣题解

---
+++ src/leetcode/simple/Solution_389.java
@@ -0,0 +1,86 @@
+package leetcode.simple;
+
+import java.util.Arrays;
+
+/**
+ * 找不同
+ * 给定两个字符串 s 和 t ，它们只包含小写字母。
+ * 字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+ * 请找出在 t 中被添加的字母。
+ * 示例 1：
+ * 输入：s = "abcd", t = "abcde"
+ * 输出："e"
+ * 解释：'e' 是那个被添加的字母。
+ * 示例 2：
+ * 输入：s = "", t = "y"
+ * 输出："y"
+ * 提示：
+ * 0 <= s.length <= 1000
+ * t.length == s.length + 1
+ * s 和 t 只包含小写字母
+ */
+public class Solution_389 {
+    public char findTheDifference(String s, String t) {
+        int n = s.length();
+        char[] chars1 = s.toCharArray();
+        char[] chars2 = t.toCharArray();
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+        for (int i = 0; i < n; i++) {
+            if (chars1[i] != chars2[i]) {
+                return chars2[i];
+            }
+        }
+        return chars2[n];
+    }
+}
+
+/*
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] cnt = new int[26];
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            cnt[ch - 'a']++;
+        }
+        for (int i = 0; i < t.length(); ++i) {
+            char ch = t.charAt(i);
+            cnt[ch - 'a']--;
+            if (cnt[ch - 'a'] < 0) {
+                return ch;
+            }
+        }
+        return ' ';
+    }
+}
+*/
+
+/*
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int as = 0, at = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            as += s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); ++i) {
+            at += t.charAt(i);
+        }
+        return (char) (at - as);
+    }
+}
+*/
+
+/*
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int ret = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            ret ^= s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); ++i) {
+            ret ^= t.charAt(i);
+        }
+        return (char) ret;
+    }
+}
+*/
