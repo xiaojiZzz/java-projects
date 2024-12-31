678a9ee4dc687c2b76ae6db0f2577da69f9c4d21
jijunwei <56964585@qq.com>
2024-12-31 12:46:04 +0800

    添加力扣 406 场周赛题解

--- src/leetcode/contest/weekly_406/Solution_3219.java
+++ src/leetcode/contest/weekly_406/Solution_3219.java
@@ -68,3 +68,23 @@ public class Solution_3219 {
         return ans;
     }
 }
+
+/*
+class Solution {
+    public long minimumCost(int m, int n, int[] horizontalCut, int[] verticalCut) {
+        Arrays.sort(horizontalCut); // 下面倒序遍历
+        Arrays.sort(verticalCut);
+        long ans = 0;
+        int i = 0;
+        int j = 0;
+        while (i < m - 1 || j < n - 1) {
+            if (j == n - 1 || i < m - 1 && horizontalCut[i] < verticalCut[j]) {
+                ans += horizontalCut[i++] * (n - j); // 上下连边
+            } else {
+                ans += verticalCut[j++] * (m - i); // 左右连边
+            }
+        }
+        return ans;
+    }
+}
+*/
