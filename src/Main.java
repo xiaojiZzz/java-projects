76c2a16254c7a96e26c72d1046493fc2f7de1706
Jackson <56964585@qq.com>
2025-03-01 10:57:12 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("jackson");
+        System.out.println("hello");
         scanner.close();
     }
 }
