dac4cba088967333ebfa45725f7b7db67d974102
jijunwei <56964585@qq.com>
2025-03-01 10:53:19 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("jackson");
+        System.out.println("jack");
         scanner.close();
     }
 }
