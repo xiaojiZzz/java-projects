be93ff569f53e493a78515d66f4a2304c05584cb
Jackson <56964585@qq.com>
2025-04-14 10:55:55 +0800

    添加力扣每日一题

--- src/leetcode/simple/Solution_1534.java
+++ src/leetcode/simple/Solution_1534.java
@@ -56,8 +56,7 @@ class Solution {
                     if (l <= r) {
                         if (l == 0) {
                             ans += sum[r];
-                        }
-                        else {
+                        } else {
                             ans += sum[r] - sum[l - 1];
                         }
                     }
@@ -65,6 +64,107 @@ class Solution {
             }
             for (int k = arr[j]; k <= 1000; ++k) {
                 ++sum[k];
+            }
+        }
+        return ans;
+    }
+}
+*/
+
+/*
+class Solution {
+    // 树状数组
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int n = arr.length, max = Arrays.stream(arr).max().getAsInt();
+        int[] sum = new int[max + 2];
+        BinaryIndexedTree binaryIndexedTree = new BinaryIndexedTree(sum);
+        int ans = 0;
+        for (int j = 0; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                if (Math.abs(arr[j] - arr[k]) <= b) {
+                    int lj = arr[j] - a, rj = arr[j] + a;
+                    int lk = arr[k] - c, rk = arr[k] + c;
+                    int l = Math.max(0, Math.max(lj, lk)), r = Math.min(max, Math.min(rj, rk));
+                    if (l <= r) {
+                        ans += binaryIndexedTree.prefixSum(r + 1) - binaryIndexedTree.prefixSum(l);
+                    }
+                }
+            }
+            binaryIndexedTree.add(arr[j] + 1, 1);
+        }
+        return ans;
+    }
+
+    static class BinaryIndexedTree {
+        private int n;
+        // 原数组
+        private int[] nums;
+        // 树状数组 从 1 开始记录
+        private int[] tree;
+
+        public BinaryIndexedTree(int[] nums) {
+            this.nums = nums;
+            this.n = nums.length;
+            this.tree = new int[n + 1];
+        }
+
+        public int lowBit(int x) {
+            return x & -x;
+        }
+
+        public void add(int x, int val) {
+            while (x <= n) {
+                tree[x] += val;
+                x += lowBit(x);
+            }
+        }
+
+        public int prefixSum(int x) {
+            int sum = 0;
+            while (x > 0) {
+                sum += tree[x];
+                x -= lowBit(x);
+            }
+            return sum;
+        }
+    }
+}
+*/
+
+/*
+class Solution {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        Integer[] idx = new Integer[arr.length];
+        Arrays.setAll(idx, i -> i);
+        Arrays.sort(idx, (i, j) -> arr[i] - arr[j]);
+
+        int ans = 0;
+        for (int j : idx) {
+            int y = arr[j];
+            List<Integer> left = new ArrayList<>();
+            for (int i : idx) {
+                if (i < j && Math.abs(arr[i] - y) <= a) {
+                    left.add(arr[i]);
+                }
+            }
+
+            List<Integer> right = new ArrayList<>();
+            for (int k : idx) {
+                if (k > j && Math.abs(arr[k] - y) <= b) {
+                    right.add(arr[k]);
+                }
+            }
+
+            int k1 = 0;
+            int k2 = 0;
+            for (int x : left) {
+                while (k2 < right.size() && right.get(k2) <= x + c) {
+                    k2++;
+                }
+                while (k1 < right.size() && right.get(k1) < x - c) {
+                    k1++;
+                }
+                ans += k2 - k1;
             }
         }
         return ans;
