5a843aa221b1d816b9e33c48c4a115e819b8dd46
Jackson <56964585@qq.com>
2025-06-08 18:42:56 +0800

    添加力扣题解

--- src/leetcode/medium/Solution_15.java
+++ src/leetcode/medium/Solution_15.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 /**
+ * 三数之和
  * 给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，
  * 同时还满足 nums[i] + nums[j] + nums[k] == 0 。请你返回所有和为 0 且不重复的三元组。
  * 注意：答案中不可以包含重复的三元组。
@@ -18,14 +18,14 @@ import java.util.List;
  * nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
  * 不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
  * 注意，输出的顺序和三元组的顺序并不重要。
+ * 提示：
+ * 3 <= nums.length <= 3000
+ * -105 <= nums[i] <= 105
  */
 public class Solution_15 {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> lists = new ArrayList<>();
         int len = nums.length;
-        if (nums == null || len < 3) {
-            return lists;
-        }
         Arrays.sort(nums);
         for (int i = 0; i < len - 2; i++) {
             if (nums[i] > 0) {
@@ -57,41 +57,3 @@ public class Solution_15 {
         return lists;
     }
 }
-
-/*
-public class Solution_15 {
-    //暴力
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<Integer> list = new ArrayList<>();
-        List<List<Integer>> lists = new ArrayList<>();
-        int len = nums.length;
-        if (len < 3) {
-            return lists;
-        }
-        Arrays.sort(nums);
-        for (int i = 0; i < len - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            for (int j = i + 1; j < len - 1; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) {
-                    continue;
-                }
-                for (int k = j + 1; k < len; k++) {
-                    if (k > j + 1 && nums[k] == nums[k - 1]) {
-                        continue;
-                    }
-                    if (nums[i] + nums[j] + nums[k] == 0) {
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(nums[k]);
-                        lists.add(new ArrayList<>(list));
-                        list.clear();
-                    }
-                }
-            }
-        }
-        return lists;
-    }
-}
-*/
