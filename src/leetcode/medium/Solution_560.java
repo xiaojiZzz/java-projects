c8ee4694340558c3137bbb2e862167944c62a890
Jackson <56964585@qq.com>
2025-06-09 23:08:46 +0800

    添加力扣题解

--- src/leetcode/medium/Solution_560.java
+++ src/leetcode/medium/Solution_560.java
@@ -2,8 +2,8 @@ package leetcode.medium;
 
 import java.util.HashMap;
 
-
 /**
+ * 和为 K 的子数组
  * 给你一个整数数组 nums 和一个整数 k ，请你统计并返回 该数组中和为 k 的子数组的个数 。
  * 子数组是数组中元素的连续非空序列。
  * 示例 1：
@@ -12,6 +12,10 @@ import java.util.HashMap;
  * 示例 2：
  * 输入：nums = [1,2,3], k = 3
  * 输出：2
+ * 提示：
+ * 1 <= nums.length <= 2 * 104
+ * -1000 <= nums[i] <= 1000
+ * -107 <= k <= 107
  */
 public class Solution_560 {
     public int subarraySum(int[] nums, int k) {
@@ -28,48 +32,3 @@ public class Solution_560 {
         return count;
     }
 }
-
-/*
-class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int cnt = 0;
-        int len = nums.length;
-        for (int i = 0; i < len; i++) {
-            int sum = 0;
-            for (int j = i; j < len; j++) {
-                sum += nums[j];
-                if (sum == k) {
-                    cnt++;
-                }
-            }
-        }
-        return cnt;
-    }
-}
-*/
-
-/*
-class Solution {
-    // 复杂度高
-    public int subarraySum(int[] nums, int k) {
-        int len = nums.length;
-        // 计算前缀和数组
-        int[] preSum = new int[len + 1];
-        preSum[0] = 0;
-        for (int i = 0; i < len; i++) {
-            preSum[i + 1] = preSum[i] + nums[i];
-        }
-
-        int count = 0;
-        for (int left = 0; left < len; left++) {
-            for (int right = left; right < len; right++) {
-                // 区间和 [left..right]，注意下标偏移
-                if (preSum[right + 1] - preSum[left] == k) {
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
-}
-*/
