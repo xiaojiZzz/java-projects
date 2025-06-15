17033a9043d692cfc22837f06d47d43a33b64742
Jackson <56964585@qq.com>
2025-06-15 10:40:09 +0800

    添加力扣题解

--- src/leetcode/simple/Solution_206.java
+++ src/leetcode/simple/Solution_206.java
@@ -1,11 +1,15 @@
 package leetcode.simple;
 
-
 /**
+ * 反转链表
  * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
  * 示例 1：
  * 输入：head = [1,2,3,4,5]
  * 输出：[5,4,3,2,1]
+ * 提示：
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 <= Node.val <= 5000
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
  */
 public class Solution_206 {
     public ListNode reverseList(ListNode head) {
@@ -23,10 +27,10 @@ public class Solution_206 {
 
 /*
 class Solution {
-    //利用栈
+    // 利用栈
     public ListNode reverseList(ListNode head) {
         Stack<ListNode> stack = new Stack<>();
-        //把链表节点全部摘掉放到栈中
+        // 把链表节点全部摘掉放到栈中
         while (head != null) {
             stack.push(head);
             head = head.next;
@@ -35,16 +39,29 @@ class Solution {
             return null;
         ListNode node = stack.pop();
         ListNode dummy = node;
-        //栈中的结点全部出栈，然后重新连成一个新的链表
+        // 栈中的结点全部出栈，然后重新连成一个新的链表
         while (!stack.isEmpty()) {
             ListNode tempNode = stack.pop();
             node.next = tempNode;
             node = node.next;
         }
-        //最后一个结点就是反转前的头结点，一定要让他的next
-        //等于空，否则会构成环
+        // 最后一个结点就是反转前的头结点，一定要让他的next等于空，否则会构成环
         node.next = null;
         return dummy;
+    }
+}
+*/
+
+/*
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
     }
 }
 */
