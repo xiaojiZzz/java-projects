a272ef6b492df54f0439a62b6e27fc9f6805160b
Jackson <56964585@qq.com>
2025-06-16 12:20:10 +0800

    添加力扣题解

--- src/leetcode/medium/Solution_2.java
+++ src/leetcode/medium/Solution_2.java
@@ -11,39 +11,23 @@ package leetcode.medium;
  * 解释：342 + 465 = 807.
  */
 public class Solution_2 {
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode listNode = new ListNode();
-        ListNode list = listNode;
-        int flag = 0;
-        while (l1 != null && l2 != null) {
-            listNode.next = new ListNode();
-            listNode = listNode.next;
-            int sum = l1.val + l2.val + flag;
-            listNode.val = sum % 10;
-            flag = sum / 10;
-            l1 = l1.next;
-            l2 = l2.next;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            if (l1 != null) {
+                carry += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                carry += l2.val;
+                l2 = l2.next;
+            }
+            cur = cur.next = new ListNode(carry % 10);
+            carry /= 10;
         }
-        while (l1 != null) {
-            listNode.next = new ListNode();
-            listNode = listNode.next;
-            int sum = l1.val + flag;
-            listNode.val = sum % 10;
-            flag = sum / 10;
-            l1 = l1.next;
-        }
-        while (l2 != null) {
-            listNode.next = new ListNode();
-            listNode = listNode.next;
-            int sum = l2.val + flag;
-            listNode.val = sum % 10;
-            flag = sum / 10;
-            l2 = l2.next;
-        }
-        if (flag == 1) {
-            listNode.next = new ListNode(1);
-        }
-        return list.next;
+        return dummy.next;
     }
 }
 
@@ -51,18 +35,15 @@ public class Solution_2 {
 class Solution {
     // 递归
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int tmp = 0;
-        return anNode(l1, l2, tmp);
+        return anNode(l1, l2, 0);
     }
 
     private ListNode anNode(ListNode l1, ListNode l2, int tmp) {
         if (l1 != null || l2 != null) {
             int num = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + tmp;
             return new ListNode(num % 10, anNode(l1 == null ? null : l1.next, l2 == null ? null : l2.next, num / 10));
-        } else {
-            if (tmp != 0) {
-                return new ListNode(tmp);
-            }
+        } else if (tmp != 0) {
+            return new ListNode(tmp);
         }
         return null;
     }
