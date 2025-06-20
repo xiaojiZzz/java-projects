df166716a76adb9cc0170e654dee6c4f7d4ba9cf
Jackson <56964585@qq.com>
2025-06-20 16:51:55 +0800

    添加力扣题解

--- src/leetcode/difficulty/Solution_23.java
+++ src/leetcode/difficulty/Solution_23.java
@@ -126,6 +126,41 @@ class Solution {
 /*
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
+        int m = lists.length;
+        if (m == 0) {
+            return null;
+        }
+        for (int step = 1; step < m; step *= 2) {
+            for (int i = 0; i < m - step; i += step * 2) {
+                lists[i] = mergeTwoLists(lists[i], lists[i + step]);
+            }
+        }
+        return lists[0];
+    }
+
+    // 21. 合并两个有序链表
+    private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(); // 用哨兵节点简化代码逻辑
+        ListNode cur = dummy; // cur 指向新链表的末尾
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                cur.next = list1; // 把 list1 加到新链表中
+                list1 = list1.next;
+            } else { // 注：相等的情况加哪个节点都是可以的
+                cur.next = list2; // 把 list2 加到新链表中
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = list1 != null ? list1 : list2; // 拼接剩余链表
+        return dummy.next;
+    }
+}
+*/
+
+/*
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
         int n = lists.length;
         if (n == 0) {
             return null;
